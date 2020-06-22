8ebc556d0f465269000b9780dcfc23e970294635
rjarmill <Robert.Jarmillo@phoenix.edu>
2020-06-22 16:09:29 -0700

    Calculator LWC

---
+++ LWC_Udemy_Course/force-app/main/default/lwc/calculatorComponent/calculatorComponent.js
@@ -0,0 +1,24 @@
+import { LightningElement } from 'lwc';
+
+export default class CalculatorComponent extends LightningElement {
+    number1;
+    number2;
+
+    handleChangeEvent(event){
+        const val = event.target.value;
+        const name = event.target.name;
+        if(name === 'number1'){
+            this.number1 = val;
+        }else{
+            this.number2 = val;
+        }
+    }
+    doSum(){
+        const sum = parseInt(this.number1) + parseInt(this.number2);
+        alert(sum);
+    }
+    doSubt(){
+        const subt = parseInt(this.number1) - parseInt(this.number2);
+        alert(subt);
+    }
+}
